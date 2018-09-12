e1b0df659997cbaeb27f043a94c355b6858c694a
insil321@hotmail.com <insil321@hotmail.com>
2018-09-12 06:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,17 @@ function dec(n){
 
 function makeInt(n){
   parseInt(n)
+  return NaN === true
 }
+
+
+it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
 
 function preserveDecimal(n){
   parseFloat(n)
